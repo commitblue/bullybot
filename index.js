9d15c10b240ec30220a88737cd198673856f135a
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-07-13 23:13:08 +0200

    new

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // MAKARON GAMES DOODOO FART
 const {Client, Intents, Permissions} = requrie("discord.js")
+const modulesPath = "./Modules"
+const commandsModule = require(modulesPath + "commands.js")
 
 Client.on("MessageCreate", (msg) => {
-    
+
 })
