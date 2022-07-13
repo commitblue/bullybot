48494fc2a2e12abb8e211129e5253653d2cb5ebe
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-07-13 23:17:43 +0200

    a

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // MAKARON GAMES DOODOO FART
 const {Client, Intents, Permissions} = requrie("discord.js")
+const client = new Client({Intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
 const modulesPath = "./Modules"
 const commandsModule = require(modulesPath + "commands.js")
+
+const prefix = "$"
 
 Client.on("MessageCreate", (msg) => {
 
