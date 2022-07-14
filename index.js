1db47aebcafe66dc19331f73031fed3bf0ffaea2
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-07-14 19:04:26 +0200

    o

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("dotenv").config()
 
 const prefix = "bullybot$"
 
-client.on("MessageCreate", (msg) => {
+client.on("message", (msg) => {
     console.log("Message")
     if (msg.content.substring(1, prefix.length) === prefix){
         console.log("Logs")
