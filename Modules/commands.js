a08025f408278baf4722a3b4f3ddf434d8adc332
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-07-14 14:17:09 +0200

    lol

--- Modules/commands.js
+++ Modules/commands.js
@@ -1,5 +1,24 @@
 const {readFileSync, writeFileSync} = require("fs")
 let settingsOfServers = require("./JSON/settingsOfServers.json")
 module.exports = {
-
+    activateBullying : {
+        description : "Activates bullying in this server.",
+        example : "bullybot$activateBullying",
+        arguments : "<none>",
+        functionToRun : (msg) => {
+            settingsOfServers[msg.guild.id].bullymode = true
+            writeFileSync("./JSON/settingsOfServers.json", settingsOfServers)
+            msg.reply("Activated bullymode.")
+        }
+    },
+    deactivateBullying : {
+        description : "Deactivates bullying in this server.",
+        example : "bullybot$deactivateBullying",
+        arguments : "<none>",
+        functionToRun : (msg) => {
+            settingsOfServers[msg.guild.id].bullymode = true
+            writeFileSync("./JSON/settingsOfServers.json", settingsOfServers)
+            msg.reply("Deactivated bullymode.")
+        }
+    }
 }
