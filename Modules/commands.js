6aa678179adfca24446eec901fdd3789e7409594
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-07-14 14:10:06 +0200

    lol

--- Modules/commands.js
+++ Modules/commands.js
@@ -1,5 +1,5 @@
 const {readFileSync, writeFileSync} = require("fs")
-let settingsOfServers = require("bullybot/JSON/settingsOfServers.json")
+let settingsOfServers = require("././JSON/settingsOfServers.json")
 module.exports = {
 
 }
